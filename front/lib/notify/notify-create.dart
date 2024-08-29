ca62bf715e6e14dda81b7d683dba8d167d4cdf9d
Carolina Fujii <carolinayumi89@gmail.com>
2024-08-29 14:28:36 -0300

    🔔 Envio de notificação

--- front/lib/notify/notify-create.dart
+++ front/lib/notify/notify-create.dart
@@ -28,89 +28,88 @@ class _CreateNotificationWidgetState extends State<CreateNotificationWidget> {
       body: SingleChildScrollView(
         child: Container(
           padding: const EdgeInsets.all(20.0),
+          height: MediaQuery.of(context).size.height,
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter,
               colors: [
                 Color(0xFFFFD700),
-                Color.fromARGB(255, 255, 255, 255),
+                Color.fromARGB(255, 241, 235, 209),
               ],
             ),
           ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               const Icon(
                 Icons.notifications,
                 size: 100.0,
                 color: Colors.black,
               ),
-              const SizedBox(height: 20),
-
-              // Campo de Texto da Notificação
-              Center(
-                child: _buildTextField(
-                  'Texto da Notificação',
-                  (value) {
-                    setState(() {
-                      texto = value;
-                    });
-                  },
-                ),
-              ),
-              const SizedBox(height: 20),
-
-              ElevatedButton(
-                onPressed: () async {
-                  final response = await http.post(
-                    Uri.parse('http://localhost:3000/createNotificacao'),
-                    headers: <String, String>{
-                      'Content-Type': 'application/json; charset=UTF-8',
-                    },
-                    body: jsonEncode(<String, dynamic>{
-                      'idUser': widget.idUser,
-                      'texto': texto,
-                      'dataHora': dataHora.toIso8601String(),
-                      'isRead': isRead,
-                    }),
-                  );
-
-                  // Dialog de sucesso ou erro
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: Text(
-                            response.statusCode == 200 ? 'Parabéns!' : 'Erro'),
-                        content: Text(response.statusCode == 200
-                            ? 'Notificação criada com sucesso!'
-                            : 'Erro ao criar notificação.'),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                              if (response.statusCode == 200) {
-                                Navigator.pop(context);
-                              }
-                            },
-                            child: const Text('OK'),
-                          ),
-                        ],
-                      );
-                    },
-                  );
+              const SizedBox(height: 15),
+              _buildTextField(
+                'Texto da Notificação',
+                (value) {
+                  setState(() {
+                    texto = value;
+                  });
                 },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFFFFD700),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(100),
+              ),
+              const SizedBox(height: 30),
+              SizedBox(
+                width: double.infinity, // Deixa o botão com largura total
+                child: ElevatedButton(
+                  onPressed: () async {
+                    final response = await http.post(
+                      Uri.parse('http://localhost:3000/createNotificacao'),
+                      headers: <String, String>{
+                        'Content-Type': 'application/json; charset=UTF-8',
+                      },
+                      body: jsonEncode(<String, dynamic>{
+                        'idUser': widget.idUser,
+                        'texto': texto,
+                        'dataHora': dataHora.toIso8601String(),
+                        'isRead': isRead,
+                      }),
+                    );
+
+                    // Diálogo de sucesso ou erro
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text(response.statusCode == 200 ? 'Parabéns!' : 'Erro'),
+                          content: Text(response.statusCode == 200
+                              ? 'Notificação criada com sucesso!'
+                              : 'Erro ao criar notificação.'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                                if (response.statusCode == 200) {
+                                  Navigator.pop(context);
+                                }
+                              },
+                              child: const Text('OK'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFFFFD700),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    elevation: 5.0,
                   ),
-                ),
-                child: const Text(
-                  'Notificar',
-                  style: TextStyle(color: Colors.black),
+                  child: const Text(
+                    'Notificar',
+                    style: TextStyle(color: Colors.black),
+                  ),
                 ),
               ),
             ],
@@ -122,25 +121,19 @@ class _CreateNotificationWidgetState extends State<CreateNotificationWidget> {
 
   Widget _buildTextField(String hintText, Function(String) onChanged,
       {bool obscureText = false}) {
-    return Container(
-      width: MediaQuery.of(context).size.width * 0.8, // Aumenta o tamanho do campo para 80% da largura da tela
-      child: TextField(
-        obscureText: obscureText,
-        style: const TextStyle(color: Colors.black),
-        maxLines: 5, // Define o campo de texto para ser maior
-        decoration: InputDecoration(
-          labelText: hintText,
-          alignLabelWithHint: true, // Alinha o texto com o topo do campo
-          border: const OutlineInputBorder(
-            borderRadius: BorderRadius.all(
-              Radius.circular(20), // Caixas de texto com bordas arredondadas
-            ),
-          ),
-          filled: true,
-          fillColor: Colors.white, // Cor de fundo das caixas
+    return TextField(
+      obscureText: obscureText,
+      style: const TextStyle(color: Colors.black),
+      maxLines: 5, // Define o campo de texto para ser maior
+      decoration: InputDecoration(
+        labelText: hintText,
+        border: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(100)),
         ),
-        onChanged: onChanged,
+        filled: true,
+        fillColor: Colors.transparent,
       ),
+      onChanged: onChanged,
     );
   }
 }
